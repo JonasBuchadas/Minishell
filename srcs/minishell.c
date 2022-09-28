07ba2175770733ca7d56c9c8f429eda952ebd4c0
JonasBuchadas <jonasmcbuchadas@gmail.com>
2022-09-28 23:46:41 +0100

    Testing with readline function

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -4,8 +4,21 @@ static void	init_minishell(int argc, char **argv, char **envp);
 
 int	main(int argc, char **argv, char **envp)
 {
+	char	*input;
+
 	init_minishell(argc, argv, envp);
-	readline(PROMPT);
+	while (1)
+	{
+		input = readline(PROMPT);
+		if (ft_strequal(input, "exit"))
+			break ;
+		if (input && *input)
+			add_history(input);
+		if (ft_strequal(input, "clear"))
+			rl_clear_history();
+		else
+			printf("Input is: %s\n", input);
+	}
 	return (0);
 }
 
