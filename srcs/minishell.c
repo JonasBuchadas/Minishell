45a4c9e429eace49d90cbf4db9f3fc6461a51873
JonasBuchadas <jonasmcbuchadas@gmail.com>
2022-10-22 23:01:28 +0100

    Create tokens from Input OK

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -2,8 +2,8 @@
 
 static void	init_minishell(char **envp);
 static void command_paths(void);
-static void exec_input(void);
-static char *find_command(char *cmd, char **paths);
+// static void exec_input(void);
+// static char *find_command(char *cmd, char **paths);
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -27,13 +27,14 @@ int	main(int argc, char **argv, char **envp)
 			create_tokens();
 			// printf("%s\n",ms()->tokens);
 			ft_lstiter(ms()->tokens, print_token);
-			exec_input();
+			// exec_input();
 		}
-		waitpid(-1, NULL, 0);
+		// waitpid(-1, NULL, 0);
 		ft_strdel(&ms()->input);
 		ft_lstclear(&ms()->tokens, &del_token);
 	}
 	ft_strdel(&ms()->input);
+	ft_lstclear(&ms()->tokens, &del_token);
 	// ft_lstclear(&ms()->tokens, &del_token);
 	return (EXIT_SUCCESS);
 }
@@ -57,44 +58,44 @@ static void command_paths(void)
 	ms()->env_paths = ft_split(ms()->envp[i] + 5, ':');
 }
 
-static void	exec_input(void)
-{
-	ms()->pid_cmd = fork();
-	if (ms()->pid_cmd == ERROR)
-		program_errors("FORK", true);
-	if (ms()->pid_cmd == CHILD_PROCESS)
-	{
-		ms()->cmd_args = ft_split(ms()->input, ' ');
-		if (access(ms()->cmd_args[0], F_OK) != ERROR)
-			execve(ms()->cmd_args[0], ms()->cmd_args, ms()->envp);
-		ms()->cmd = find_command(ms()->cmd_args[0], ms()->env_paths);
-		if (!ms()->cmd || access(ms()->cmd, F_OK) == ERROR)
-			command_errors(ms()->cmd_args[0], true);
-		execve(ms()->cmd, ms()->cmd_args, ms()->envp);
-	}
-}
+// static void	exec_input(void)
+// {
+// 	ms()->pid_cmd = fork();
+// 	if (ms()->pid_cmd == ERROR)
+// 		program_errors("FORK", true);
+// 	if (ms()->pid_cmd == CHILD_PROCESS)
+// 	{
+// 		ms()->cmd_args = ft_split(ms()->input, ' ');
+// 		if (access(ms()->cmd_args[0], F_OK) != ERROR)
+// 			execve(ms()->cmd_args[0], ms()->cmd_args, ms()->envp);
+// 		ms()->cmd = find_command(ms()->cmd_args[0], ms()->env_paths);
+// 		if (!ms()->cmd || access(ms()->cmd, F_OK) == ERROR)
+// 			command_errors(ms()->cmd_args[0], true);
+// 		execve(ms()->cmd, ms()->cmd_args, ms()->envp);
+// 	}
+// }
 
-static char *find_command(char *cmd, char **paths)
-{
-	char *path;
-	char *tmp;
-	size_t i;
+// static char *find_command(char *cmd, char **paths)
+// {
+// 	char *path;
+// 	char *tmp;
+// 	size_t i;
 
-	i = 0;
-	while (paths[i])
-	{
-		tmp = ft_strjoin(paths[i], "/");
-		path = ft_strjoin(tmp, cmd);
-		ft_strdel(&tmp);
-		if (access(path, F_OK) == 0)
-		{
-			ft_strarray_clear(&paths);
-			return (path);
-		}
-		else
-			ft_strdel(&path);
-		i++;
-	}
-	ft_strarray_clear(&paths);
-	return (NULL);
-}
+// 	i = 0;
+// 	while (paths[i])
+// 	{
+// 		tmp = ft_strjoin(paths[i], "/");
+// 		path = ft_strjoin(tmp, cmd);
+// 		ft_strdel(&tmp);
+// 		if (access(path, F_OK) == 0)
+// 		{
+// 			ft_strarray_clear(&paths);
+// 			return (path);
+// 		}
+// 		else
+// 			ft_strdel(&path);
+// 		i++;
+// 	}
+// 	ft_strarray_clear(&paths);
+// 	return (NULL);
+// }
--- srcs/parser_prompt.c
+++ srcs/parser_prompt.c
@@ -11,8 +11,8 @@ void create_tokens(void)
 	int			i;
 
 	ms()->last_i = 0;
-	i = -1;
-	while (ms()->input[++i])
+	i = 0;
+	while (ms()->input[i])
 	{
 		if (ms()->input[i] == '\"')
 			i = handle_double_quotes(i);
@@ -20,6 +20,8 @@ void create_tokens(void)
 			i = handle_single_quotes(i);
 		else if (ms()->input[i] == ' ')
 			i = handle_space(i);
+		if (!(ms()->input[i] == '\"' || ms()->input[i] == '\''))
+			i++;
 	}
 	handle_space(i);
 }
@@ -35,8 +37,12 @@ static int	handle_double_quotes(int i)
 	{
 		if (ms()->input[i] == '\"')
 		{
-			add_token(start, len, DOUBLE_QUOTES);
-			ms()->last_i = ++i;
+			if (len > 0)
+				add_token(start, len, DOUBLE_QUOTES);
+			if(ms()->input[i + 1] && ms()->input[i + 1] != ' ')
+				ms()->last_i = ++i;
+			else
+				ms()->last_i = ++i + 1;
 			return i;
 		}
 		len++;
@@ -55,8 +61,12 @@ static int	handle_single_quotes(int i)
 	{
 		if (ms()->input[i] == '\'')
 		{
-			add_token(start, len, SINGLE_QUOTES);
-			ms()->last_i = ++i;
+			if (len > 0)
+				add_token(start, len, SINGLE_QUOTES);
+			if (ms()->input[i + 1] && ms()->input[i + 1] != ' ')
+				ms()->last_i = ++i;
+			else
+				ms()->last_i = ++i + 1;
 			return i;
 		}
 		len++;
@@ -68,23 +78,10 @@ static int	handle_single_quotes(int i)
 static int	handle_space(int i)
 {
 	size_t			len;
-	// unsigned int	space_len;
 
 	len = i - ms()->last_i;
 	if (len > 0)
-	// {
-	// 	while (input[ms()->last_i] == ' ')
-	// 		ms()->last_i++;
-	// 	while (input[ms()->last_i + len] && input[ms()->last_i + len] != ' ')
-	// 		len++;
-	// 	if (len != 0)
-	// 		add_token(ms()->last_i, len, NORMAL);
-	// 	ms()->last_i += (int)len;
-	// }
 	{
-		// space_len = 0;
-		// while (ms()->input[ms()->last_i + space_len] == ' ')
-		// 	space_len++;
 		add_token(ms()->last_i, len, NORMAL);
 		ms()->last_i += len + 1;
 	}
